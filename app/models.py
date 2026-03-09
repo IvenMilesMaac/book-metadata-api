b14ffd39ed915595c3df17f1ceb83be7aba14d46
IvenMilesMaac <sc23imm@leeds.ac.uk>
2026-03-09 17:19:16 +0000

    define sqlalchemy models

--- app/models.py
+++ app/models.py
@@ -17,13 +17,6 @@ book_category = Table(
     Column("category_id", Integer, ForeignKey("category.id"), primary_key=True)
 )
 
-user_favorites = Table(
-    "user_favorites",
-    Base.metadata,
-    Column("user_id", Integer, ForeignKey("user.id"), primary_key=True),
-    Column("book_id", Integer, ForeignKey("book.id"), primary_key=True)
-)
-
 class Book(Base):
     __tablename__ = "book"
 
@@ -35,8 +28,6 @@ class Book(Base):
     # Relationships
     authors = relationship("Author", secondary=book_author, back_populates="books")
     categories = relationship("Category", secondary=book_category, back_populates="books")
-    ratings = relationship("Ratings", back_populates="book", cascade="all, delete-orphan")
-    favorited_by = relationship("User", secondary="user_favorites", back_populates="favorites")
 
 class Author(Base):
     __tablename__ = "author"
@@ -55,27 +46,3 @@ class Category(Base):
 
     # Relationship to Books
     books = relationship("Book", secondary=book_category, back_populates="categories")
-
-class User(Base):
-    __tablename__ = "user"
-
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
-
-    #Relationships
-    ratings = relationship("Ratings", back_populates="user", cascade="all, delete-orphan")
-    favorites = relationship("Book", secondary="user_favorites", back_populates="favorited_by")
-
-# Association table with extra information
-class Ratings(Base):
-    __tablename__ = "ratings"
-
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
-    book_id = Column(Integer, ForeignKey("book.id"), nullable=False)
-    rating = Column(Float, nullable=False)
-
-    # Relationships
-    user = relationship("User", back_populates="ratings")
-    book = relationship("Book", back_populates="ratings")
